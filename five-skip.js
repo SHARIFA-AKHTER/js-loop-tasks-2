e54e0ce462a12ca9effd8938331b449a70821743
unknown <sr0589071@gmail.com>
2024-09-02 18:21:51 +0600

    create odd-skip-one.js file

---
+++ five-skip.js
@@ -0,0 +1,12 @@
+// for (var num = 55; num <= 85; num++){
+//     if (num % 2 === 0 || num % 5 ===0 ) {
+//         continue;
+//     }
+//     console.log(num);
+// }
+
+for (let i =55; i <= 85; i++){
+       if(i % 5 === 0  || i%5 === 0){
+             console.log(i);
+         }
+   }
